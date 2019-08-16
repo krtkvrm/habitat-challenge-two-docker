0507e8f3b3b79d0617706728befca8266662dce7
kartik.verma <kartik.verma@go-jek.com>
2019-08-16 17:39:51 +0530

    [Kartik] Proto file for Multiple Container policy

---
+++ environment.py
@@ -0,0 +1,17 @@
+import argparse
+from baselines.agents.simple_agents import get_agent_cls
+import habitat
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--agent-class", type=str, default="GoalFollower")
+    args = parser.parse_args()
+
+    agent = get_agent_cls(args.agent_class)(success_distance=0.2)
+    challenge = habitat.Challenge()
+    challenge.submit(agent)
+
+
+if __name__ == "__main__":
+    main()
+
